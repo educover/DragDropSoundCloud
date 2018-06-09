2ed2675bd774e270b9511ea561ea291217da9b55
Edu Alcover <alcover.edu@gmail.com>
2018-06-09 12:34:06 +0200

    proyecto finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,12 @@ SC.initialize({
     client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb'
 })
 
-let pintarImagen=(tracks, i)=>`<div class='imagen_mini col-2'><img src=
-${tracks[i].artwork_url} id = ${tracks[i].id} draggable='true' ondragstart='onDragStart(event)'></div>"
+let pintarImagen=(tracks, i)=>`<div class='imagen_mini col-2 img'><img src=
+${tracks[i].artwork_url} id = ${tracks[i].id} draggable='true' ondragstart='onDragStart(event)'></div>
 `
-
-
+let pintarImagenDefecto=(tracks, i)=>`<div class='imagen_mini col-2 img'><img src=
+'./assets/imag/img_r.jpg' id = ${tracks[i].id} draggable='true' ondragstart='onDragStart(event)'></div>
+`
 
 function miFunc() {
     let val = $('input').val();
@@ -14,14 +15,16 @@ function miFunc() {
     SC.get('/tracks', {
         q:val
     }).then(function(tracks){
-        for (let i = 0; i < 7; i++) {
-            $('.music').append(pintarImagen(tracks, i));  
+        for (let i = 0; i < 10; i++) {
+            if(tracks[i].artwork_url===null){
+                $('.music').append(pintarImagenDefecto(tracks, i));
+            } else {
+            $('.music').append(pintarImagen(tracks, i));
+            }
             console.log(tracks);
         }
     });
 }
-
-
 
 function onDragOver(e){
     e.preventDefault();
@@ -36,13 +39,13 @@ function onDragStart(e, miMusic){
 
 function onDrop(e){
    // console.log(e)
-   var data = e.dataTransfer.getData('id');
-   console.log(data)
-   
+    var data = e.dataTransfer.getData('id');
+    e.target.appendChild(document.getElementById(data));
     SC.stream('/tracks/'+data)
     .then(function(player){
     player.play();
     }
+    
     )}
 
 
